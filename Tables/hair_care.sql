fd35fb8eec1a9c16dce4049f414c6cdf69490deb
Ratnapriya200109 <102627340+Ratnapriya200109@users.noreply.github.com>
2022-04-02 19:02:10 +0530

    Add files via upload

---
+++ Tables/hair_care.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 02, 2022 at 03:23 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `itsabtu`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hair_care`
+--
+
+CREATE TABLE `hair_care` (
+  `SNo` int(10) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `Description` text NOT NULL,
+  `Category` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `hair_care`
+--
+
+INSERT INTO `hair_care` (`SNo`, `Name`, `Description`, `Category`, `image`) VALUES
+(1, 'Hair Oils/ Hair Masks', '', 'videos', 'haircare.jpeg'),
+(2, 'Home Remedies And Treatments', '', 'videos', 'haircare.jpeg'),
+(3, 'Hair Diet', '', 'videos', 'haircare.jpeg');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
