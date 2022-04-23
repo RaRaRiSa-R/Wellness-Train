8834994e738f9d567456b2bdc6c9268cc135d0f3
risharma123 <102652600+risharma123@users.noreply.github.com>
2022-04-23 18:38:43 +0530

    Add files via upload

---
+++ cat.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/categories.css">
+     <link rel="stylesheet" href="css/image-bar-style.css">
+     <link rel="stylesheet" type="text/css" href="css/footer.css"> 
+</head>
+<body class="back">
+    <?php
+    include('navbar.php');
+?><div class="hed">
+  <h3>Choose any categories<h3></div>
+    <div class="wrap">
+  
+    <div class="d">
+       
+        <a href="#" class="border"><img src="images\diet.jpg" alt=""></a>
+        <p  class="b">Diabetics</p>
+    </div>
+    <div class="d">
+        <a href="#" class="border"><img src="images/kneepain.jpg" alt=""></a>
+        <p class="b">Knee Pain</p>
+    </div>
+
+    <div class="d">
+        <a href="#" class="border"><img src="images/weightloss.jpg" alt=""></a>
+        <p class="b">weight Loss</p>
+    </div>
+    <div class="d">
+        <a href="#" class="border"><img src="images/muscleloss.jpg" alt=""></a>
+        <p class="b">Muscle Loss</p>
+    </div>
+    <div class="d">
+        <a href="#" class="border"><img src="images/musclegain1.jpg" alt=""></a>
+        <p class="b">Muscle Gain</p>
+    </div>
+    <div class="d">
+        <a href="#" class="border"><img src="images/bp.jpg" alt=""></a>
+        <p class="b">Blood Pressure</p>
+    </div>
+    <div class="d">
+        <a href="#" class="border"><img src="images/acid.jpg" alt=""></a>
+        <p class="b">Acidity</p>
+    </div>
+    <div class="d">
+        <a href="#" class="border"><img src="images/menstruation1.jpg" alt=""></a>
+        <p class="b">Menstruation</p>
+    </div>
+    <div class="d">
+        <a href="#" class="border"><img src="images/biotinsake1.jpg" alt=""></a>
+        <p class="b">Pubic part</p>
+    </div>
+    <div class="d">
+        <a href="#" class="border"><img src="images/eyecare.jpg" alt=""></a>
+        <p class="b">Eye Care</p>
+    </div>
+    <div class="d">
+        <a href="#" class="border"><img src="images/nail.jpg" alt=""></a>
+        <p class="b">Nail Care</p>
+    </div>
+
+    </div>
+    <footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="footer-col">
+                <ul>
+                    <li><a href="contactus.php">Contact Us</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+             <ul>
+                 <li><a href="otherS.php">Our Services</a></li>
+             </ul>
+         </div>
+            <div class="footer-col">
+                <ul>
+                    <li><a href="faq.php">FAQ</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <div class="social-links">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    </footer>
+</body>
+</html>
